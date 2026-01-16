318f1f8317f2b12c6c43a3ff1b3da2dd108affdd
Zobayer Abdullah <zobayer.abdullah9999@gmail.com>
2026-01-16 19:35:51 +0600

    Update BelongsToTenant.php

--- app/Traits/BelongsToTenant.php
+++ app/Traits/BelongsToTenant.php
@@ -11,7 +11,7 @@ trait BelongsToTenant
     protected static function bootBelongsToTenant(): void
     {
         static::addGlobalScope(new TenantScope);
-
+ 
         static::creating(function ($model) {
             if (! $model->tenant_id && app()->bound('current_tenant_id')) {
                 $model->tenant_id = app('current_tenant_id');
